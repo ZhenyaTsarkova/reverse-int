af4243518858ec6c0bdb2d6c6b00e8ff7c114b1d
ZhenyaTsarkova <TsarkovaZhenya@gmail.com>
2022-01-10 13:43:24 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   let result = '';
+   let resultNum = 0;
+   if (n >= 0) {
+      let num = n.toString();
+      for (let i = 0; i < num.length; i += 1) {
+         result = `${num[i]}${result}`;
+      }
+      resultNum = Number(result);
+   } else {
+      let num = n.toString().substr(1);
+      for (let i = 0; i < num.length; i += 1) {
+         result = `${num[i]}${result}`;
+      }
+      result = `-${result}`
+      resultNum = - Number(result);
+   }
+   return resultNum;
 }
